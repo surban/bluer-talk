285747cbaa9895fd545c8607b3f6ab50735d2e32
Sebastian Urban <surban@surban.net>
2023-04-20 14:35:41 +0000

    client addr

--- blush_client.sh
+++ blush_client.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-exec l2cat connect -r E4:5F:01:49:DD:D8 240
+if [ "$(hostname)" = "ubupi4a" ] ; then
+    exec l2cat connect -r E4:5F:01:49:DD:D8 240
+else
+    exec l2cat connect -r DC:A6:32:F9:C9:F8 240
+fi

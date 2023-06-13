f02293e56db408c27e16ff5ef5e1aaf69d457040
Sebastian Urban <surban@surban.net>
2023-06-13 16:22:20 +0200

    Deps

--- blush_client.sh
+++ blush_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(hostname)" = "ubupi4a" ] ; then
+if [ "$(hostname)" = "ubupi4a_" ] ; then
     exec l2cat connect -r E4:5F:01:49:DD:D8 240
 else
     exec l2cat connect -r DC:A6:32:F9:C9:F8 240
